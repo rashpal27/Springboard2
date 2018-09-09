3fe6f26a622863f8c785da426d239adb75455fd5
Rashida Muckadam <rlakhat@Rashidas-MBP-4.attlocal.net>
2018-09-09 11:51:08 -0700

    titanic done

--- titanic_springboad.R
+++ titanic_springboad.R
@@ -4,26 +4,27 @@ library(tidyverse)
 
 
 library(readr)
-titanic_original <- read_csv("Desktop/titanic_original.csv")
+titanic_original <- read_csv("~/Desktop/springboard_titanic/titanic_original.csv")
 View(titanic_original)
 
 
 
 titanic_original1 <- titanic_original
 
-
 titanic_original1$boat[which (is.na(titanic_original1$boat))] <-"None"
 
 titanic_original1 <-add_column(titanic_original1, has_cabin_number = "missing")
 
-#titanic_original1$has_cabin_number<- "missing"
+titanic_original1$has_cabin_number <- ifelse (is.na(titanic_original1$cabin), 0, 1)
+
+titanic_original1$embarked [which (is.na(titanic_original1$embarked))] <- "S"
+#ifelse (is.na(titanic_original1$embarked), "missin", 7) # for debug
 
 
-
-#titanic_original1$has_cabin_number<- ifelse(
-  
+#mean(titanic_original1$age) to get the mean
 
 
+titanic_original1$age[which (is.na(titanic_original1$age))] <- mean(titanic_original1$age , na.rm= TRUE)
 
-
+write_csv(titanic_original1,"titanic_clean.csv")
 
